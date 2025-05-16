9ac43286d3317d1484676da11b6b2d3ffac7daac
FernandoCruzCavina <fernando.cruz.cavina@gmail.com>
2025-05-15 22:05:04 -0300

    feat: add service registry

---
+++ service-registry/src/test/java/dev/fernando/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fernando.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
